54c27a685bd6589aad5c0163c3f3d89b47e62ef3
s0me1newithhand7s <litvinovb0@gmail.com>
2025-04-27 13:19:07 +0300

    git: new settings {signing, lfs, aliases}

--- hand7s/programs/git.nix
+++ hand7s/programs/git.nix
@@ -1,9 +1,26 @@
-{...}: {
+{
+    config,
+    ...
+}: {
     programs = {
         git = {
             enable = true;
             userEmail = "117505144+s0me1newithhand7s@users.noreply.github.com";
             userName = "s0me1newithhand7s";
+            aliases = {
+                pushall = "push github; push gitlab";
+                pushall-f = "push --force github; push --force gitlab";
+                key = config.sops.secrets.sshKeyOpen.path;
+            };
+
+            signing = {
+                format = "ssh";
+                signByDefault = true;
+            };
+
+            lfs = {
+                enable = true;
+            };
         };
     };
 }
