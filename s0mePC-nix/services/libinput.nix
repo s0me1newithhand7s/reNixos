8d6a7550fd699fcf4bc230fbbca12d162bfe1f2e
s0me1newithhand7s <litvinovb0@gmail.com>
2025-05-16 21:46:57 +0300

    s0mePC-nix: bringing normal button mapping (new mouse, hooray)

--- s0mePC-nix/services/libinput.nix
+++ s0mePC-nix/services/libinput.nix
@@ -5,7 +5,7 @@
             mouse = {
                 accelProfile = "flat";
                 accelSpeed = "-1.0";
-                buttonMapping = "1 3 2 4 5 6 7 8 9";
+                buttonMapping = "1 2 3 4 5 6 7 8 9";
                 horizontalScrolling = true;
                 leftHanded = false;
                 naturalScrolling = true;
