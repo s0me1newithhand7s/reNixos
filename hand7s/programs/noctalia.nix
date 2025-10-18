b33c17079f8f735cbac6170cae978ef366176cd3
s0me1newithhand7s <117505144+s0me1newithhand7s@users.noreply.github.com>
2025-10-19 01:12:09 +0300

    noctalia.nix: full config
    Signed-off-by: s0me1newithhand7s <117505144+s0me1newithhand7s@users.noreply.github.com>

--- hand7s/programs/noctalia.nix
+++ hand7s/programs/noctalia.nix
@@ -1,14 +1,46 @@
 {config, ...}: {
     programs =
-        if (config.home.gui.sessionType == "Hyprland")
+        if (
+            config.home.gui.sessionType == "Hyprland"
+            )
         then {
             noctalia-shell = {
                 enable = true;
                 settings = {
+                    general = {
+                        avatarImage = "/home/hand7s/Pictures/pfp4_mini.jpg";
+                        animationDisabled = false;
+                        dimDesktop = false;
+                        forceBlackScreenCorners = true;
+                        showScreenCorners = true;
+                        screenRadiusRatio = 2;
+                    };
+
+                    appLauncher = {
+                        position = "center";
+                        useApp2Unit = false;
+                        sortByMostUsed = true;
+                        enableClipboardHistory = false;
+                        terminalCommand = "ghostty -e";
+                    };
+
+                    audio = {
+                        visualizerType = "mirrored";
+                        volumeOverdrive = true;
+                        cavaFrameRate = 165;
+                        volumeStep = 5;
+                    };
+
                     bar = {
-                        density = "compact";
+                        floating = true;
+                        density = "comfortable";
                         position = "right";
                         showCapsule = false;
+                        marginVertical = 1;
+                        marginHorizontal = 0.6;
+                        monitors = [
+                            "DP-3"
+                        ];
 
                         widgets = {
                             left = [
@@ -19,18 +51,25 @@
 
                                 {
                                     id = "Workspace";
+                                    hideUnoccupied = true;
+                                    labelMode = "index";
                                 }
 
                                 {
                                     id = "MediaMini";
+                                    autoHide = true;
+                                    scrollingMode = "hover";
+                                    showAlbumArt = true;
+                                    showVisualizer = true;
+                                    visualizerType = "mirrored";
                                 }
                             ];
 
                             center = [
                                 {
-                                    formatHorizontal = "HH:mm";
-                                    formatVertical = "HH mm";
                                     id = "Clock";
+                                    formatHorizontal = "⏱HH:mm:ss";
+                                    formatVertical = "⏱ HH mm ss";
                                     useMonospacedFont = true;
                                     usePrimaryColor = true;
                                 }
@@ -38,18 +77,123 @@
 
                             right = [
                                 {
+                                    id = "KeyboardLayout";
+                                    displayMode = "onhover";
+                                }
+
+                                {
+                                    id = "Microphone";
+                                    displayMode = "onhover";
+                                }
+
+                                {
                                     id = "Volume";
+                                    displayMode = "onhover";
+                                }
+
+                                {
+                                    id = "Spacer";
                                 }
 
                                 {
                                     id = "Tray";
+                                    blacklist = [ ];
+                                    colorizeIcons = true;
                                 }
 
                                 {
                                     id = "NotificationHistory";
+                                    hideWhenZero = false;
+                                    showUnreadBadge = true;
                                 }
                             ];
                         };
+                    };
+
+                    controlCenter = {
+                        position = "close_to_bar_button";
+                        quickSettingsStyle = "compact";
+                        widgets = {
+                            quickSettings = [
+                                {
+                                    id = "Notifications";
+                                }
+
+                                {
+                                    id = "ScreenRecorder";
+                                }
+
+                                {
+                                    id = "KeepAwake";
+                                }
+
+                                {
+                                    id = "NightLight";
+                                }
+                            ];
+                        };
+                    };
+
+                    dock = {
+                        displayMode = "auto_hide";
+                        floatingRatio = 1;
+                        onlySameOutput = true;
+                        colorizeIcons = true;
+                        monitors = [
+                            "DP-3"
+                        ];
+                    };
+
+                    colors = with config.lib.stylix.colors; {
+                        mBackground = base00;
+                        mSurface = base01;
+                        mOverlay = base02;
+                        mMuted = base03;
+                        mBorder = base04;
+                        mText = base05;
+                        mTextAlt = base06;
+                        mTextDim = base07;
+                        mError = base08;
+                        mWarning = base09;
+                        mAccent = base0A;
+                        mSuccess = base0B;
+                        mCyan = base0C;
+                        mPrimary = base0D;
+                        mMagenta = base0E;
+                        mOrange = base0F;
+                        mOnBackground = base05;
+                        mOnSurface = base05;
+                        mOnPrimary = base00;
+                        mOnError = base00;
+                    };
+
+                    colorSchemes = {
+                        darkMode = true;
+                        useWallpaperColors = false;
+                        generateTemplatesForPredefined = false;
+                        predefinedScheme = "Tokyo Night";
+                        matugenSchemeType = "scheme-fruit-salad";
+                    };
+
+                    osd = {
+                        enabled = true;
+                        alwaysOnTop = true;
+                        autoHideMs = 2500;
+                        location = "top";
+                        monitors = [
+                            "DP-3"
+                        ];
+                    };
+
+                    location = {
+                        name = "Yelaguba";
+                        showWeekNumberInCalendar = true;
+                        use12hourFormat = false;
+                        useFahrenheit = false;
+                    };
+
+                    network = {
+                        wifiEnabled = false;
                     };
                 };
             };
