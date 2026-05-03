14eea58fbba7e4576286a0638c59958d190723bf
s0me1newithhand7s <git+me@hand7s.org>
2026-05-03 15:28:47 +0300

    kyra(hardening): tmp hardening
    Signed-off-by: s0me1newithhand7s <git+me@hand7s.org>

--- kyra/boot/tmp.nix
+++ kyra/boot/tmp.nix
@@ -2,6 +2,10 @@ _: {
   boot = {
     tmp = {
       cleanOnBoot = true;
+      useZram = true;
+      useTmpfs = true;
+      tmpfsSize = "50%";
+      tmpfsHugeMemoryPages = "within_size";
     };
   };
 }
