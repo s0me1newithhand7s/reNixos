c563897f0280183ab202947c497c53dba85551fd
s0me1newithhand7s <git+me@hand7s.org>
2026-05-03 15:43:59 +0300

    kyra(hardening): nh gc timer init
    Signed-off-by: s0me1newithhand7s <git+me@hand7s.org>

--- kyra/programs/nh.nix
+++ kyra/programs/nh.nix
@@ -2,6 +2,14 @@ _: {
   programs = {
     nh = {
       enable = true;
+      clean = {
+        enable = true;
+        dates = "daily";
+        extraArgs = [
+          "-k 2"
+          "-K 1d"
+        ];
+      };
     };
   };
 }
