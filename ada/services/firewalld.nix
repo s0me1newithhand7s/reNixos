ab9fff95a139355757e4bdc584ad00157991bd2b
s0me1newithhand7s <git+me@hand7s.org>
2026-05-29 00:14:49 +0300

    chore(ada): firewalld hardening
    Signed-off-by: s0me1newithhand7s <git+me@hand7s.org>

--- ada/services/firewalld.nix
+++ ada/services/firewalld.nix
@@ -3,8 +3,21 @@ _: {
     firewalld = {
       enable = true;
 
+      settings = {
+        IPv6_rpfilter = "strict";
+        CleanupModulesOnExit = true;
+        StrictForwardPorts = true;
+        logDenied = "off";
+        FlushAllOnReload = "yes";
+        ReloadPolicy = "DROP";
+        RFC3964_IPv4 = "yes";
+        NftablesCounters = "no";
+        NftablesTableOwner = "yes";
+        IndividualCalls = "no";
+      };
+
       zones = {
-        "eno1" = {
+        "wan" = {
           interfaces = [
             "ens1"
           ];
