23357404ded45c2ceb6db8518cb8d6679a9306ef
s0me1newithhand7s <git+me@hand7s.org>
2026-05-29 00:13:35 +0300

    chore(ada): gamescope flags update
    Signed-off-by: s0me1newithhand7s <git+me@hand7s.org>

--- ada/programs/gamescope.nix
+++ ada/programs/gamescope.nix
@@ -5,6 +5,19 @@
       package = pkgs.gamescope;
       args = [
         "-e"
+
+        "-W 2560"
+        "-H 1440"
+
+        "-r 165"
+        "-o 15"
+        "--adaptive-sync"
+
+        "--backend sdl"
+        "--expose-wayland"
+        "--xwayland-count 2"
+
+        "--force-grab-cursor"
       ];
     };
   };
