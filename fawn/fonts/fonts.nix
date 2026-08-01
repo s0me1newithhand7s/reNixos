a85931ba7ccffb01968642da474c3cfcd1649ad3
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:43:16 +0300

    chore(fawn): slim down fonts packages
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- fawn/fonts/fonts.nix
+++ fawn/fonts/fonts.nix
@@ -7,14 +7,6 @@
     packages = with pkgs; [
       nerd-fonts.monaspace
       nerd-fonts.noto
-      nerd-fonts.iosevka-term-slab
-      dejavu_fonts
-      freefont_ttf
-      gyre-fonts
-      liberation-fonts
-      unifont
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
     ];
   };
 }
