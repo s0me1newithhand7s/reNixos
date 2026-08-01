3efdeca381cac5710a22943043b5f3028dc3ab25
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:06:37 +0300

    fix(ada): remove documentation
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

---
+++ ada/documentation/documentation.nix
@@ -0,0 +1,25 @@
+_: {
+  documentation = {
+    enable = false;
+
+    dev = {
+      enable = false;
+    };
+
+    doc = {
+      enable = false;
+    };
+
+    info = {
+      enable = false;
+    };
+
+    man = {
+      enable = false;
+    };
+
+    nixos = {
+      enable = false;
+    };
+  };
+}
