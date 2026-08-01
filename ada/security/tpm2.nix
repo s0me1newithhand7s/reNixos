97efe847c6422caa06f5d9571cb1ada3949a8a58
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:18:55 +0300

    feat(ada): initialize TPM2-related configuration
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

---
+++ ada/security/tpm2.nix
@@ -0,0 +1,19 @@
+_: {
+  security = {
+    tpm2 = {
+      enable = true;
+
+      abrmd = {
+        enable = true;
+      };
+
+      pkcs11 = {
+        enable = true;
+      };
+
+      tctiEnvironment = {
+        enable = true;
+      };
+    };
+  };
+}
