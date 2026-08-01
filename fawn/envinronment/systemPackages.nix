aa1397997072caec80621b2485fd910e08c27ba0
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:25:57 +0300

    fix(fawn): make systemPackages non-forced
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- fawn/envinronment/systemPackages.nix
+++ fawn/envinronment/systemPackages.nix
@@ -1,44 +1,33 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   environment = {
-    systemPackages = with pkgs;
-      lib.mkForce [
-        kdb
-        attr
-        bzip
-        cpio
-        curl
-        getent
-        getconf
-        gzip
-        xz
-        less
-        libcap
-        ncurses
-        libressl
-        mkpasswd
-        shadow
-        time
-        zstd
-        bashInteractive
-        gawk
-        gnugrep
-        kmod
-        systemd-tmpfiles
-        glibc-locales
-        finit
-        finix-logo
-        uutils-acl
-        uutils-coreutils-uutils-coreutils-noprefix
-        uutils-findutils
-        uutils-diffutils
-        uutils-procps
-        uutils-sed
-        uutils-tar
-        uutils-util-linux
-      ];
+    systemPackages = with pkgs; [
+      attr
+      bashInteractive
+      bind
+      curlFull
+      gawk
+      getconf
+      getent
+      gzip
+      iproute2
+      kmod
+      libcap
+      libressl
+      mkpasswd
+      ncurses
+      openssh
+      patch
+      shadow
+      su
+      util-linux
+      uutils-acl
+      uutils-coreutils-noprefix
+      uutils-findutils
+      uutils-hostname
+      uutils-procps
+      uutils-sed
+      uutils-tar
+      xz
+    ];
   };
 }
