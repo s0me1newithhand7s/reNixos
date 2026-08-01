89625c6b66fe755c3ea43b80a92d3456ebf990ae
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:21:09 +0300

    fix(ada): expand ntpd-rs config
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- ada/services/ntpd-rs.nix
+++ ada/services/ntpd-rs.nix
@@ -5,18 +5,37 @@ _: {
       useNetworkingTimeServers = false;
 
       settings = {
+        source-defaults = {
+          poll-interval-limits = {
+            min = 6;
+            max = 9;
+          };
+        };
+
+        synchronization = {
+          minimum-agreeing-sources = 1;
+          accumulated-step-panic-threshold = 3600;
+          single-step-panic-threshold = 1800;
+          startup-step-panic-threshold = {
+            forward = "inf";
+            backward = 1800;
+          };
+        };
+
         source = [
           {
             mode = "nts";
             address = "time.cloudflare.com";
           }
+
           {
             mode = "nts";
             address = "nts.netnod.se";
           }
+
           {
             mode = "nts";
-            address = "ptbtime1.ptb.de";
+            address = "nts.ntp.se";
           }
         ];
       };
