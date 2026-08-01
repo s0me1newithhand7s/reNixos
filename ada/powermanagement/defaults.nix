9229c2d65cca98c7a23e02140d25d47b82ff433b
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:10:19 +0300

    chore(ada): move powermanagement udev rules to actual module
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

---
+++ ada/powermanagement/defaults.nix
@@ -0,0 +1,5 @@
+_: {
+  powerManagement = {
+    cpuFreqGovernor = "performance";
+  };
+}
--- ada/services/udev.nix
+++ ada/services/udev.nix
@@ -4,11 +4,6 @@
       packages = [
         pkgs.yubikey-personalization
       ];
-
-      extraRules = ''
-        SUBSYSTEM=="cpu", ACTION=="add", ATTR{cpufreq/scaling_governor}="performance"
-        SUBSYSTEM=="cpu", ACTION=="add", ATTR{cpufreq/energy_performance_preference}="performance"
-      '';
     };
   };
 }
