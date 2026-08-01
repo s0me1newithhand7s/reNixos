d9db96427bc13c50c62b6b55ce0f2b90f43f9013
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:27:18 +0300

    feat(fawn): add /home subvol
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- fawn/fileSystems/filesystems.nix
+++ fawn/fileSystems/filesystems.nix
@@ -1,25 +1,39 @@
 _: {
   fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-partlabel/root";
-      fsType = "btrfs";
-      options = [
-        "compress=zstd"
-        "noatime"
-        "discard=async"
-        "subvol=/root"
-      ];
-    };
-
     "/boot" = {
       device = "/dev/disk/by-partlabel/boot";
       fsType = "vfat";
       options = [
         "dmask=0077"
         "fmask=0077"
+        "noexec"
         "nosuid"
         "nodev"
         "rw"
+      ];
+    };
+
+    "/" = {
+      device = "/dev/disk/by-partlabel/root";
+      fsType = "btrfs";
+      options = [
+        "compress=zstd:3"
+        "noatime"
+        "discard=async"
+        "subvol=/root"
+      ];
+    };
+
+    "/home" = {
+      device = "/dev/disk/by-partlabel/root";
+      fsType = "btrfs";
+      options = [
+        "compress=zstd:3"
+        "noatime"
+        "discard=async"
+        "subvol=/home"
+        "nosuid"
+        "nodev"
       ];
     };
 
@@ -27,7 +41,7 @@ _: {
       device = "/dev/disk/by-partlabel/root";
       fsType = "btrfs";
       options = [
-        "compress=zstd"
+        "compress=zstd:3"
         "noatime"
         "discard=async"
         "subvol=/nix"
