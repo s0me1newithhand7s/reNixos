4e62a33d1bb81bf81ccb09f8714d8a2c5a807372
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 00:20:15 +0300

    chore(ada): update firewalld config
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- ada/services/firewalld.nix
+++ ada/services/firewalld.nix
@@ -4,26 +4,101 @@ _: {
       enable = true;
 
       settings = {
-        IPv6_rpfilter = "strict";
+        IPv6_rpfilter = "loose";
         CleanupModulesOnExit = true;
-        StrictForwardPorts = true;
-        logDenied = "off";
         FlushAllOnReload = true;
-        ReloadPolicy = "DROP";
         RFC3964_IPv4 = true;
         NftablesCounters = false;
-        NftablesTableOwner = true;
         IndividualCalls = false;
+      };
+
+      services = {
+        "sunshine" = {
+          ports = [
+            {
+              port = 47984;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47985;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47986;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47987;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47988;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47989;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47990;
+              protocol = "tcp";
+            }
+
+            {
+              port = 48010;
+              protocol = "tcp";
+            }
+
+            {
+              port = 47998;
+              protocol = "udp";
+            }
+
+            {
+              port = 47999;
+              protocol = "udp";
+            }
+
+            {
+              port = 48000;
+              protocol = "udp";
+            }
+
+            {
+              port = 48002;
+              protocol = "udp";
+            }
+
+            {
+              port = 48010;
+              protocol = "udp";
+            }
+          ];
+        };
       };
 
       zones = {
         "wan" = {
           interfaces = [
-            "ens1"
+            "eno1"
           ];
 
           services = [
             "sunshine"
+            "steam-streaming"
+          ];
+
+          ports = [
+            {
+              port = 8082;
+              protocol = "tcp";
+            }
           ];
         };
       };
