71e9df6ebb5a4bba4b82f898c95084d9a888f0c9
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 16:11:29 +0300

    feat(isla): initialize secure boot
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- isla/boot/loader/systemd-boot.nix
+++ isla/boot/loader/systemd-boot.nix
@@ -1,18 +1,8 @@
-{lib, ...}: {
+_: {
   boot = {
     loader = {
       systemd-boot = {
-        enable = lib.mkDefault false;
-        sortKey = "nixos";
-        memtest86 = {
-          enable = true;
-          sortKey = "o_memtest86";
-        };
-
-        netbootxyz = {
-          enable = false;
-          sortKey = "o_netbootxyz";
-        };
+        enable = false;
       };
     };
   };
