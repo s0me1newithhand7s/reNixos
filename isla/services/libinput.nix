42ae92a6b28cdd430d2ce399afa88e18c884387a
s0me1newithhand7s <s0me1newithhand7s@gmail.com>
2026-08-02 16:28:34 +0300

    chore(isla): remove touchpad configs
    Signed-off-by: s0me1newithhand7s <s0me1newithhand7s@gmail.com>

--- isla/services/libinput.nix
+++ isla/services/libinput.nix
@@ -4,19 +4,11 @@ _: {
       enable = true;
       mouse = {
         accelProfile = "flat";
-        accelSpeed = "-1.0";
+        accelSpeed = "1";
         buttonMapping = "1 2 3 4 5 6 7 8 9";
         horizontalScrolling = true;
         leftHanded = false;
         naturalScrolling = true;
-      };
-
-      touchpad = {
-        accelProfile = "adaptive";
-        accelSpeed = "-1.0";
-        buttonMapping = "1 2 3 4 5 6 7 8 9";
-        clickMethod = "clickfinger";
-        leftHanded = false;
       };
     };
   };
